6626590558f53f52f214eb9c96afb9971b836b01
Denis Stebunov <denis.stebunov@ivelum.com>
2014-05-06 23:23:20 +0400

    support kwargs for Playlist.find_by_ids

--- pybrightcove/playlist.py
+++ pybrightcove/playlist.py
@@ -178,14 +178,14 @@ class Playlist(object):
 
     @staticmethod
     def find_by_ids(ids, connection=None, page_size=100, page_number=0,
-        sort_by=DEFAULT_SORT_BY, sort_order=DEFAULT_SORT_ORDER):
+        sort_by=DEFAULT_SORT_BY, sort_order=DEFAULT_SORT_ORDER, **kwargs):
         """
         List playlists by specific IDs.
         """
         ids = ','.join([str(i) for i in ids])
         return pybrightcove.connection.ItemResultSet('find_playlists_by_ids',
             Playlist, connection, page_size, page_number, sort_by, sort_order,
-            playlist_ids=ids)
+            playlist_ids=ids, **kwargs)
 
     @staticmethod
     def find_by_reference_ids(reference_ids, connection=None, page_size=100,
